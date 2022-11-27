bfd4eeb597baf9bdaca6a4b2e58e05a1e3bc316f
KimHajun <river0105@naver.com>
2022-11-27 16:59:36 +0900

    최댓값

---
+++ 최댓값.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut m = 0;
+    let mut index = 0;
+    for i in 1..=9 {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).expect("Failed to read line.");
+        let n: i32 = line.trim().parse().expect("Failed to parse line.");
+        if m < n {
+            index = i;
+            m = n;
+        }
+    }
+    println!("{}", m);
+    println!("{}", index);
+}
